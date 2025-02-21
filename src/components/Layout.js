cb4e16e35d0d4efc1e342a7c3f43a35c64ad4270
AdityavardhanKoya <adityavardhan.koya@gmail.com>
2025-02-22 00:14:45 +0530

    Modified Changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Analytics } from "@vercel/analytics/react"; // Import Analytics
 
-const Layout = ({children, className=""}) => {
+const Layout = ({ children, className = "" }) => {
   return (
     <div className={`w-full h-full z-0 inline-block p-32 bg-light ${className}`}>
-        {children}
+      {children}
+      <Analytics /> {/* Add Analytics here */}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ import { AnimatePresence } from "framer-motion";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Layout from "@/components/Layout";
+
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -21,7 +23,9 @@ export default function App({ Component, pageProps }) {
       >
         <NavBar />
         <AnimatePresence mode="wait">
+          <Layout>
           <Component key={router.asPath} {...pageProps} />
+          </Layout>
         </AnimatePresence>
         <Footer />
       </main>
